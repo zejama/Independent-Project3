ac3e4b71ecf28cc810ddfa477f72b1cd55bf2dff
Zeinab Jama <zejama99@gmail.com>
2021-07-05 00:41:08 -0700

    made minor adjustments but not getting an output

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,37 @@
 //Business Logic
 
-function roboger(userInput) {
-  const numArray = [];
-  numInput = parseInt(userInput);
+function roboger(usersInput) {
+  let numArray = [];
+  numInput = parseInt(usersInput);
   for (let i = 0; i <= numInput; i++) {
     numArray.push(i)
   }
-
   const inputArray = numArray.map(function(num) {
     return num.toString();
   });
-
+  
   const exceptionArray = inputArray.map (function(userInput) {
     if (userInput.includes("3") === true) {
       return userInput = "Won't you be my neighbor?";
-    } else if (userInput.includes("2") === true) {
+  } else if (userInput.includes("2") === true) {
       return userInput = "Boop!";
-    } else if (userInput.includes("1") === true) {
+  } else if (userInput.includes("1") === true) {
       return userInput = "Beep!";
-    } 
+  } else {
+      return userInput;
+    }
   });
+  const combineOutputs = exceptionArray.join(", ");
+  return combineOutputs;
 
-}
-
-
-
-
-
+};
 
 
 //User Interface Logic
 $(document).ready(function() {  
-  $("userInput").submit(function(event) {
+  $("#userInput").submit(function(event) {
     event.preventDefault();
-
     const numInput = $("input#input1").val();
-
-  })
-})
+    $("div#output").show (roboger(userInput))
+  });
+});
