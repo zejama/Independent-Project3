04dff4ce7705707bfc5c26700c676213e6bdd5d8
Zeinab Jama <zejama99@gmail.com>
2021-07-04 19:24:11 -0700

    add some UI logic and begin writing business logic

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Business Logic
+
+function 
+
+
+
+
+
+
+
+//User Interface Logic
+$(document).ready(function() { 
+  $("userInput").submit(function(event) {
+    event.preventDefault();
+
+    const numInput = $("input#input1").val();
+
+  })
+})
